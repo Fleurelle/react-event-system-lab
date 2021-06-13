fa75ae63c51155022d7a6bd240ce7a75619a8d08
Rose <r.d.jeanlouis01@gmail.com>
2021-06-13 00:07:02 -0400

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+import 'raf/polyfill';
+
+class EyesOnMe extends React.Component {
+    handleFocusEvent = () => {
+        console.log('Good!')
+    }
+
+    handleBlurEvent = () => {
+        console.log('Hey! Eyes on me!')
+    }
+    render() {
+        return (
+            <div>
+                <button onFocus={this.handleFocusEvent} onBlur={this.handleBlurEvent}></button>
+    
+            </div>
+        )
+    }
+}
+
+export default EyesOnMe;
